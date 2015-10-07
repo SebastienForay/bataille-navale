4a5afb1922466fbb431680d54a0b8f5a90dcf210
EtienneTr <tienou42@gmail.com>
2015-10-07 17:14:56 +0200

    début détection

--- interface.py
+++ interface.py
@@ -7,7 +7,7 @@ class Interface():
 	def __init__(self):
 		self.letterLabel = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 		self.root = Tk()
-		self.initTab()
+		self.allTab = self.initTab()
 		self.root.mainloop()
 
 	def initTab(self):
@@ -23,7 +23,8 @@ class Interface():
 
 		#canvas du tableau
 		canvas = Canvas(tableau1, width=550, height=550, borderwidth=2, bg="white")
-
+		canvas.focus_set()
+		canvas.bind("<Button-1>", self.placeBateau)
 		#label chiffre
 
 		txt = 1
@@ -53,6 +54,9 @@ class Interface():
 		tableau2.pack(side=LEFT, padx=25, pady=5)
 
 		canvas2 = Canvas(tableau2, width=550, height=550, borderwidth=2, bg="white")
+		canvas2.focus_set()
+		canvas2.bind("<Button-1>", self.placeBateau)
+
 		rectangle2 = canvas2.create_rectangle(50, 550, 550, 50)
 
 		#label chiffre
@@ -77,8 +81,33 @@ class Interface():
 			canvas2.create_line(50, line, 550, line)
 
 		canvas2.pack()
+		return cadre
 
-	#def placeBateau(self):
+	def placeBateau(self,event):
+		x = event.x
+		y = event.y
+		print(x,y)
+		if x > 50 and y > 50:
+			for line in range(50,550,50):
+				for col in range(50,550,50):
+					if x > line and x < line+50:
+						if y > col and y < col+50:
+							print(line,col)
+							for children in self.root.children.values():
+								for childs in children.children.values():
+									for Unchilds in childs.children.values():
+										name = Unchilds.winfo_class
+										print(name)
+										#Unchilds.create_rectangle(line, col, line+50, col+50, background="red")
+							"""frame = self.root.frame
+							for children in frame.children.values():
+								info = children.winfo_class()
+								print(info)
+								if info == "canvas":
+									canvas.create_rectangle(line, col, line+50, col+50, bg="red")
+								break"""
 
+
+				
 
 Interface()
