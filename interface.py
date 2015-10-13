69402eb138c3827fe913d0ca94d91b5e6abb4525
EtienneTr <tienou42@gmail.com>
2015-10-13 09:00:31 +0200

    interface

--- interface.py
+++ interface.py
@@ -2,10 +2,16 @@
 #-*- coding utf-8 -*-
 
 from tkinter import *
+from tkinter import messagebox
+from class_shipInterface import Ship
 
 class Interface():
 	def __init__(self):
 		self.letterLabel = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+		self.allPos = ['D', 'R', 'U', 'L']
+		self.nbShip = 6
+		self.lastPos = 0
+		self.saveX = -1
 		self.root = Tk()
 		self.allTab = self.initTab()
 		self.root.mainloop()
@@ -25,6 +31,7 @@ class Interface():
 		canvas = Canvas(tableau1, width=550, height=550, borderwidth=2, bg="white")
 		canvas.focus_set()
 		canvas.bind("<Button-1>", self.placeBateau)
+		canvas.bind("<Button-3>", self.tournerBateau)
 		#label chiffre
 
 		txt = 1
@@ -55,7 +62,7 @@ class Interface():
 
 		canvas2 = Canvas(tableau2, width=550, height=550, borderwidth=2, bg="white")
 		canvas2.focus_set()
-		canvas2.bind("<Button-1>", self.placeBateau)
+		canvas2.bind("<Button-1>", self.lancer)
 
 		rectangle2 = canvas2.create_rectangle(50, 550, 550, 50)
 
@@ -86,39 +93,141 @@ class Interface():
 	def placeBateau(self,event):
 		x = event.x
 		y = event.y
-		print(x,y)
 		caller = event.widget
-		for line in range(50,550,50):
-			for col in range(50,550,50):
-				#clique dans le tableau
-				if x > line and x < line+50:
-					if y > col and y < col+50:
-						caller.create_rectangle(line, col, line+50, col+50, fill="red")
-		"""
-		if x > 50 and y > 50:
-			for line in range(50,550,50):
-				for col in range(50,550,50):
-					#clique dans le tableau
-					if x > line and x < line+50:
-						if y > col and y < col+50:
-							print(line,col)
-							for children in self.root.children.values():
-								for childs in children.children.values():
-									for Unchilds in childs.children.values():
-										#Unchilds = childs.children.values()
-										#name = childs.winfo_containing(x,y)
-										name = str(childs.winfo_pathname(childs.winfo_id()))
-										print(name)
-										#Unchilds.create_rectangle(line, col, line+50, col+50, background="red")
-							frame = self.root.frame
-							for children in frame.children.values():
-								info = children.winfo_class()
-								print(info)
-								if info == "canvas":
-									canvas.create_rectangle(line, col, line+50, col+50, bg="red")
-								break"""
+		#plus de bateau
+		if self.nbShip == 1:
+			messagebox.showinfo("Bateaux", "Vous avez posé tous vos bateaux !")
+		else:
+			if x > 50 and x < 550 and y > 50 and y < 550:
+				lengShip = self.nbShip
+				if lengShip > 3:
+					lengShip -= 1
 
-
+				calShip = lengShip * 50
+				#calcul du petit entier divisible par 50 le plus proche
+				entX = x // 50 * 50
+				entY = y // 50 * 50
+				pos = ""
+				#bateau bas
+				if (entY + calShip) <= 550:
+					pos = "D"
+					idPos = 0
+					rect = caller.create_rectangle(entX, entY, entX+50, entY+calShip, fill="red", tags="rect")
+				#bateau droite
+				elif (entX + calShip) <= 550:
+					pos = "R"
+					idPos = 1
+					rect = caller.create_rectangle(entX, entY, entX+calShip, entY+50, fill="red", tags="rect")
+				#bateau haut
+				else:
+					pos = "U"
+					idPos = 2
+					rect = caller.create_rectangle(entX, entY+50, entX+50, entY-calShip+50, fill="red", tags="rect")
 				
+				if self.nbShip < 6:
+					self.tmpShip.insertShip()
+				tmpShip = Ship(1, lengShip, int(x//50) , int(y//50), pos)
+
+				inc = -1
+				if tmpShip.bCreated == False:
+					caller.delete(rect)
+					self.lastPos = 5
+					self.saveX = -1
+					#try other position
+					inc = idPos + 1
+					while inc != idPos:
+						print(self.allPos[inc])
+						tmpShip = Ship(1, lengShip, int(x//50) , int(y//50), self.allPos[inc])
+						if tmpShip.bCreated == True:
+							pos = self.allPos[inc]
+							if inc == 0:
+								rect = caller.create_rectangle(entX, entY, entX+50, entY+calShip, fill="red", tags="rect")
+								self.lastPos = 0
+							elif inc == 1:
+								rect = caller.create_rectangle(entX, entY, entX+calShip, entY+50, fill="red", tags="rect")
+								self.lastPos = 1
+							elif inc == 2:
+								rect = caller.create_rectangle(entX, entY+50, entX+50, entY-calShip+50, fill="red", tags="rect")
+								self.lastPos = 2
+							else:
+								rect = caller.create_rectangle(entX+50, entY, entX-calShip+50, entY+50, fill="red", tags="rect")
+								self.lastPos = 3
+							break
+						inc += 1
+						if inc > 3:
+							inc = 0
+				
+				if tmpShip.bCreated == True:
+					#save for rotate
+					self.saveX = x
+					self.saveY = y
+					self.pos = pos
+					self.tmpShip = tmpShip
+					self.rect = rect
+					self.nbShip -= 1
+					if inc == -1:
+						self.lastPos = 0
+
+				#stop rotate
+				#if self.nbShip == 1:
+				#	self.saveX = -1
+
+	def tournerBateau(self,event):
+		if self.saveX != -1:
+			caller = event.widget
+			x = self.saveX
+			y = self.saveY
+
+			entX = x // 50 * 50
+			entY = y // 50 * 50
+			pos = self.pos
+			lengShip = self.nbShip
+			if lengShip < 3:
+				lengShip += 1
+			calShip = lengShip * 50
+			#bateau bas
+			if ((entY + calShip) <= 550) and pos != "D" and self.lastPos < 1:
+				pos = "D"
+				self.lastPos = 1
+				rect = caller.create_rectangle(entX, entY, entX+50, entY+calShip, fill="red", tags="rect")
+			#bateau droite
+			elif ((entX + calShip) <= 550) and pos != "R" and self.lastPos < 2:
+				pos = "R"
+				self.lastPos = 2
+				rect = caller.create_rectangle(entX, entY, entX+calShip, entY+50, fill="red", tags="rect")
+			#bateau haut
+			elif ((entY - calShip) >= 0) and pos != "U" and self.lastPos < 3:
+				pos = "U"
+				self.lastPos = 3
+				rect = caller.create_rectangle(entX, entY+50, entX+50, entY-calShip+50, fill="red", tags="rect")
+			elif ((entX - calShip) >=0) and pos != "L" and self.lastPos < 4:
+				pos = "L"
+				self.lastPos = 0
+				rect = caller.create_rectangle(entX+50, entY, entX-calShip+50, entY+50, fill="red", tags="rect")
+			else:
+				messagebox.showinfo("déplacer", "Impossible à déplacer !")
+				self.lastPos = 0
+				self.tournerBateau(event)
+				return
+
+			tmpShip = Ship(1, lengShip, int(x//50) , int(y//50), pos)
+			if tmpShip.bCreated == False:
+				caller.delete(rect)
+				messagebox.showinfo("déplacer", "Impossible à déplacer !")
+			else:
+				caller.delete(self.rect)
+				self.pos = pos
+				self.tmpShip = tmpShip
+				self.rect = rect
+
+	def lancer(self, event):
+		x = event.x
+		y = event.y
+		caller = event.widget
+		entX = x // 50 * 50
+		entY = y // 50 * 50
+
+		if (entX + 50 <= 550) and (entY + 50 <= 550) and (entX - 50 >= 0) and (entY - 50 >= 0):
+			rect = caller.create_rectangle(entX, entY, entX+50, entY+50, fill="red")
 
 Interface()
